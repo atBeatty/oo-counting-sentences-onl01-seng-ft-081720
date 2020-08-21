4be1f237bb8f6496242b01996786462363a92389
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-21 02:43:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,7 @@ class String
 
   def count_sentences
     sentence_array = self.split("." || "!" ||  "?")
+    binding.pry
     sentence_array.length
     # dont_count = sentence_arraycount('')
   end
